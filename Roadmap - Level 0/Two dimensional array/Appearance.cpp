615c9f8e4d5d3e1eaa35723d42b8bcbccde86788
Ngô Bá Hoàng <ngobahoang88@gmail.com>
2026-06-15 20:13:39 +0700

    tạo trước plate để chuẩn bị cày thuật toán 2d rolling hash

---
+++ Roadmap - Level 0/Two dimensional array/Appearance.cpp
@@ -0,0 +1,18 @@
+#pragma GCC optimize("03")
+#pragma GCC optimize("unroll-loops")
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+
+    
+
+
+
+
+    return 0;
+}
