74706c8c4799388d1cef2c6a8a1e10eba98c143e
Ngô Bá Hoàng <289644758+tikl20tok@users.noreply.github.com>
2026-06-22 15:27:45 +0700

    Add version 1 (worse) of "Point coverage"

---
+++ Roadmap - Level 0/Two dimensional array/Point coverage (Hard, Interval section, Cutting formula(Cf is easy)).cpp
@@ -0,0 +1,43 @@
+#pragma GCC optimize("03")
+#pragma GCC optimize("unroll-loops")
+
+#include<bits/stdc++.h>
+using namespace std;
+/*
+go from 0 to optimized first
+
+*/
+bool toado[105][105];
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+
+    long long n;
+    cin>>n;
+    long long i,j,k;
+    long long x1,y1,x2,y2;
+    memset(toado,false,sizeof(toado));
+    for (k=1;k<=n;k++)
+    {
+        cin>>x1>>y1>>x2>>y2;
+        for (i=x1;i<=x2;i++)
+            for (j=y1;j<=y2;j++)
+            {
+                toado[i][j]=true;
+            }
+    }
+    long long dem=0;
+    for (i=1;i<=100;i++)
+        for (j=1;j<=100;j++)
+        {
+            if (toado[i][j]==true)
+                dem+=1;
+        }
+    cout<<dem;
+
+
+    //worse code, version 1
+
+    return 0;
+}
