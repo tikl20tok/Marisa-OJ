f31e73bc8865638a5e6acbc78344bf3b04380dd7
Ngô Bá Hoàng <289644758+tikl20tok@users.noreply.github.com>
2026-06-25 13:04:14 +0700

    I don't know what this change for

--- Roadmap - Level 0/Two dimensional array/Point coverage (Hard, Interval section, Cutting formula(Cf is easy)).cpp
+++ Roadmap - Level 0/Two dimensional array/Point coverage (Hard, Interval section, Cutting formula(Cf is easy)).cpp
@@ -7,7 +7,7 @@ using namespace std;
 go from 0 to optimized first
 
 */
-bool toado[105][105];
+bool coordinate[105][105];
 int main()
 {
     ios::sync_with_stdio(false);
@@ -17,21 +17,21 @@ int main()
     cin>>n;
     long long i,j,k;
     long long x1,y1,x2,y2;
-    memset(toado,false,sizeof(toado));
+    memset(coordinate,false,sizeof(coordinate));
     for (k=1;k<=n;k++)
     {
         cin>>x1>>y1>>x2>>y2;
         for (i=x1;i<=x2;i++)
             for (j=y1;j<=y2;j++)
             {
-                toado[i][j]=true;
+                coordinate[i][j]=true;
             }
     }
     long long dem=0;
     for (i=1;i<=100;i++)
         for (j=1;j<=100;j++)
         {
-            if (toado[i][j]==true)
+            if (coordinate[i][j]==true)
                 dem+=1;
         }
     cout<<dem;
